52ba8d07786f3d2325d9b62cb06686a67746555f
udensidev <mmesomaudensi@gmail.com>
2025-04-10 02:45:27 -0500

    Updated customization

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,17 +4,8 @@ const Footer = () => (
   <footer className='footer'>
       <p>© {new Date().getFullYear()} Mmesoma Udensi</p>
 
-      <div className='footer__social'>
-        <a href='https://github.com/udensidev' aria-label='GitHub'>
-          <GitHubIcon />
-        </a>
-        <a href='https://linkedin.com/in/mmesomaudensi' aria-label='LinkedIn'>
-          <LinkedInIcon />
-        </a>
-      </div>
-
       <p className='footer__credit'>
-        Template by Raj Shekhar
+        Template by Raj Sheka
       </p>
     </footer>
 )
