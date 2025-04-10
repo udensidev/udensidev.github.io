0bb377b751aa6208b29c3095da9c9f26a02765ca
udensidev <mmesomaudensi@gmail.com>
2025-04-10 02:44:37 -0500

    Updated customization

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,13 +2,21 @@ import './Footer.css'
 
 const Footer = () => (
   <footer className='footer'>
-    <a
-      href='https://github.com/rjshkhr/cleanfolio'
-      className='link footer__link'
-    >
-      Created By Raj Shekhar
-    </a>
-  </footer>
+      <p>© {new Date().getFullYear()} Mmesoma Udensi</p>
+
+      <div className='footer__social'>
+        <a href='https://github.com/udensidev' aria-label='GitHub'>
+          <GitHubIcon />
+        </a>
+        <a href='https://linkedin.com/in/mmesomaudensi' aria-label='LinkedIn'>
+          <LinkedInIcon />
+        </a>
+      </div>
+
+      <p className='footer__credit'>
+        Template by Raj Shekhar
+      </p>
+    </footer>
 )
 
 export default Footer
